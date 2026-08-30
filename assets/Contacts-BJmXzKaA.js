a9acfd99e849e013ba02eab9edf0b9a030938826
StayCame <timaser605@gmail.com>
2026-08-30 11:55:18 +0300

    Deploy updated site

--- assets/Contacts-BJmXzKaA.js
+++ assets/Contacts-BJmXzKaA.js
@@ -1,4 +1,4 @@
-import{a as e}from"./rolldown-runtime-CNC7AqOf.js";import{c as t,i as n,n as r,o as i,r as a,t as o,u as s}from"./GlassCard-Dv4nyoKI.js";import{n as c}from"./index-B8OLTjiJ.js";var l=e(s(),1),u=r(),d=i.div`
+import{a as e}from"./rolldown-runtime-CNC7AqOf.js";import{c as t,i as n,n as r,o as i,r as a,t as o,u as s}from"./GlassCard-Dv4nyoKI.js";import{n as c}from"./index-DURMSr1p.js";var l=e(s(),1),u=r(),d=i.div`
   padding: 120px 0 60px;
   min-height: 60vh;
 
@@ -233,7 +233,7 @@ import{a as e}from"./rolldown-runtime-CNC7AqOf.js";import{c as t,i as n,n as r,o
               "@context": "https://schema.org",
               "@type": "LocalBusiness",
               "name": "CAR TRADE LINE",
-              "description": "Прямой импорт автомобилей и автозапчастей из Японии",
+              "description": "Прямой импорт автомобилей  из Японии",
               "url": "https://staycame.github.io/JDMvl/",
               "telephone": "+7-962-333-46-77",
               "email": "info@cartradeline.com",
